d3851be65bf2da7314a35715e738ed5bdc0be403
Julia Vancraeyveld <vancra11@gmail.com>
2021-03-16 20:40:17 +0100

    added frank and his children

--- website/route.py
+++ website/route.py
@@ -1,21 +1,32 @@
 from website import app
-
+from flask import render_template
 
 @app.route('/')
 def hello():
 
-    return """<h1 style="color:rgb(4, 135, 191);">Bonjour et bienvenue</h1>
-    <h1 style="color:rgb(4, 104, 181);">sur le site officiel des Vancraeyvelds</h1>
-    <h2 style="color:rgb(4, 81, 181);">ceci sont les membres de la famille:</h2>
-    <ul>
-             <h4 style="color:rgb(255, 3, 141);"><li>Julia Vancraeyveld</li>
-             <h4 style="color:rgb(214, 2, 140);"><li>Saartje Vancraeyveld</li></h4>
-             <h4 style="color:rgb(189, 4, 149);"><li>Michiel Vancraeyveld</li></h4>
-             <h4 style="color:rgb(186, 4, 189);"><li>Pieter Vancraeyveld</li></h4>
-             <h4 style="color:rgb(142, 4, 189);"><li>Frank Vancraeyveld</li></h4>
-             <h4 style="color:rgb(102, 4, 189);"><li>Florent Vancraeyveld</li></h4>
-             <h4 style="color:rgb(75, 4, 189);"><li>Leon Vancraeyveld</li></h4>
-    </ul>  
-    <h2 style="color:rgb(5, 8, 171);">Notre famille est connu pour notre don de pété non-stop :)</h2> """ 
+    return render_template("page-petante.html") 
 
+@app.route('/pieter')
+def pieter():
+
+    return render_template("page-pieter.html") 
     
+@app.route('/julia')
+def julia():
+
+    return render_template("page-julia.html") 
+
+@app.route('/saar')
+def saar():
+
+    return render_template("page-saar.html") 
+    
+@app.route('/michiel')
+def michiel():
+
+    return render_template("page-michiel.html")  
+
+@app.route('/frank')
+def frank():
+
+    return render_template("page-frank.html")     
